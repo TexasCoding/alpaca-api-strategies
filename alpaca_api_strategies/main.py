7900eac0c6c22a9fadd716ff846535873a6865d0
TexasCoding <jeff10278@me.com>
2024-05-19 14:14:30 -0500

    updates

--- alpaca_api_strategies/main.py
+++ alpaca_api_strategies/main.py
@@ -1,8 +1,10 @@
 from py_alpaca_daily_losers.daily_losers import DailyLosers
 
+
 def main():
     daily_losers = DailyLosers()
     daily_losers.run()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
