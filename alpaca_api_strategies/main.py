962c3261dc802a980859a95f6c7fc990905d1309
TexasCoding <jeff10278@me.com>
2024-05-23 16:41:58 -0500

    updated scheduling

--- alpaca_api_strategies/main.py
+++ alpaca_api_strategies/main.py
@@ -1,9 +1,17 @@
+import pendulum
 from py_alpaca_daily_losers.daily_losers import DailyLosers
+
+current_time = pendulum.now("America/New_York")
 
 
 def main():
     daily_losers = DailyLosers()
-    daily_losers.run()
+    if current_time.hour >= 8 and current_time.hour <= 10:
+        daily_losers.run()
+    elif current_time.hour > 10 and current_time.hour <= 16:
+        daily_losers.sell_positions_from_criteria()
+    else:
+        print("Market is closed.")
 
 
 if __name__ == "__main__":
