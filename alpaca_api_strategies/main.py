0062860a3e21668a28363b5ceae7e0100509a21f
TexasCoding <jeff10278@me.com>
2024-06-02 17:53:38 -0500

    test heroku

--- alpaca_api_strategies/main.py
+++ alpaca_api_strategies/main.py
@@ -6,13 +6,13 @@ current_time = pendulum.now("America/New_York")
 
 def main():
     daily_losers = DailyLosers()
-    if current_time.hour >= 8 and current_time.hour <= 10:
-        daily_losers.run()
-    elif current_time.hour > 10 and current_time.hour <= 16:
-        daily_losers.sell_positions_from_criteria()
-    else:
-        print("Market is closed.")
-        return
+    # if current_time.hour >= 8 and current_time.hour <= 10:
+    daily_losers.run()
+    # elif current_time.hour > 10 and current_time.hour <= 16:
+    #     daily_losers.sell_positions_from_criteria()
+    # else:
+    #     print("Market is closed.")
+    #     return
 
 
 if __name__ == "__main__":
