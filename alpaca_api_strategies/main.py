bced46b8f6694c3ff4338c16ad6a577e9223a86d
TexasCoding <jeff10278@me.com>
2024-06-17 22:00:14 -0500

    update

--- alpaca_api_strategies/main.py
+++ alpaca_api_strategies/main.py
@@ -12,7 +12,7 @@ def main():
     daily_losers = DailyLosers()
     with console.status("[bold green]Daily Losers is running, please wait..."):
         if current_time.hour >= 8 and current_time.hour <= 10:
-            daily_losers.run()
+            daily_losers.run(buy_limit=5, article_limit=3)
         elif current_time.hour > 10 and current_time.hour <= 16:
             daily_losers.close.sell_positions_from_criteria()
         else:
