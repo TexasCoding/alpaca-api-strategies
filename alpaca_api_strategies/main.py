a833e827e7ca7177a0f386efec2fd834de9df95b
TexasCoding <jeff10278@me.com>
2024-06-22 17:31:39 -0500

    update

--- alpaca_api_strategies/main.py
+++ alpaca_api_strategies/main.py
@@ -9,18 +9,18 @@ current_time = pendulum.now("America/New_York")
 
 
 def main():
-    # daily_losers = DailyLosers()
-    # with console.status("[bold green]Daily Losers is running, please wait..."):
-    #     if current_time.hour >= 8 and current_time.hour <= 10:
-    #         daily_losers.run(buy_limit=4, article_limit=4)
-    #     elif current_time.hour > 10 and current_time.hour <= 16:
-    #         daily_losers.close.sell_positions_from_criteria()
-    #     else:
-    #         print("Market is closed.")
-    #         return
     daily_losers = DailyLosers()
     with console.status("[bold green]Daily Losers is running, please wait..."):
-        daily_losers.run(buy_limit=4, article_limit=4)
+        if current_time.hour >= 8 and current_time.hour <= 10:
+            daily_losers.run(buy_limit=4, article_limit=4)
+        elif current_time.hour > 10 and current_time.hour <= 16:
+            daily_losers.close.sell_positions_from_criteria()
+        else:
+            print("Market is closed.")
+            return
+    # daily_losers = DailyLosers()
+    # with console.status("[bold green]Daily Losers is running, please wait..."):
+    #     daily_losers.run(buy_limit=4, article_limit=4)
        
 
 if __name__ == "__main__":
